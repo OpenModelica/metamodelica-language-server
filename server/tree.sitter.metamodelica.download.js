e9a964d58347a391113cfd52af35e16ffbe9a729
Adeel Asghar <adeel.asghar@liu.se>
2025-03-03 16:20:14 +0100

    Remove dev dependency of node-wget (#6)
    Directly use wget code to fetch wasm file

--- server/tree.sitter.metamodelica.download.js
+++ server/tree.sitter.metamodelica.download.js
@@ -1,5 +1,53 @@
-var wget = require('node-wget');
+// Code taken from https://github.com/TylerLeonhardt/wgetjs
+
 var downloadUrl = 'https://github.com/OpenModelica/tree-sitter-metamodelica/releases/download/v0.3.0/tree-sitter-metamodelica.wasm';
+
+var fs = require('fs'),
+  request = require('request');
+
+function wget(options, callback) {
+  if (typeof options === 'string') {
+    options  = { url: options };
+  }
+  options      = options  || {};
+  callback     = callback || function (){};
+  var src      = options.url || options.uri || options.src,
+    parts    = src.split('/'),
+    file     = parts[parts.length-1];
+  parts        = file.split('?');
+  file         = parts[0];
+  parts        = file.split('#');
+  file         = parts[0];
+  options.dest = options.dest || './';
+  if (options.dest.substr(options.dest.length-1,1) == '/') {
+    options.dest = options.dest + file;
+  }
+
+  function handle_request_callback(err, res, body) {
+    if (err) {
+      callback(err);
+    } else {
+      var data = {
+        filepath: options.dest
+      };
+      if (res && res.headers) {
+        data.headers = res.headers;
+      }
+      callback(err, data, body);
+    }
+  }
+
+  if (options.dry) {
+    handle_request_callback(null, {}, { filepath: options.dest });
+    return options.dest;
+  } else {
+    try {
+      request(options, handle_request_callback).pipe(fs.createWriteStream(options.dest));
+    } catch (err) {
+      callback(err);
+    }
+  }
+}
 
 wget({
   url:  downloadUrl,
