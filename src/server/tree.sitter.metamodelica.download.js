574ad8f8d674320147aa06f6dfc70902f30bb5de
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2025-12-05 14:53:33 +0100

    Update npm packages

--- src/server/tree.sitter.metamodelica.download.js
+++ src/server/tree.sitter.metamodelica.download.js
@@ -1,13 +1,14 @@
-const axios = require('axios');
-const fs = require('fs');
+import axios from 'axios';
+import { createWriteStream } from 'fs';
 
 var downloadUrl = 'https://github.com/OpenModelica/tree-sitter-metamodelica/releases/download/v0.3.0/tree-sitter-metamodelica.wasm';
 
-axios.get(downloadUrl, {responseType: "stream"} )
-.then(response => {
-  response.data.pipe(fs.createWriteStream("tree-sitter-metamodelica.wasm"));
-  console.log("Downloaded", downloadUrl);
-})
-.catch(error => {
-  console.log(error);
-});
+axios
+  .get(downloadUrl, {responseType: "stream"} )
+  .then(response => {
+    response.data.pipe(createWriteStream("tree-sitter-metamodelica.wasm"));
+    console.log("Downloaded", downloadUrl);
+  })
+  .catch(error => {
+    console.log(error);
+  });
